2997bfe5ac4e94db12a5a7a4ef51b744be6f7b9e
sobii <moradiyan.sobhan@gmail.com>
2020-10-04 17:41:28 +0330

    paragraph splitter

--- HW2_group8.py
+++ HW2_group8.py
@@ -3,21 +3,35 @@ from hazm import *
 
 
 def normalizing():
+
     with open("./group8text.txt", 'r', encoding="utf8") as file:
         text = file.read()
-        print("Before normalize")
-        print(text)
 
-        print("--------------------------------------------------------------------")
-        print("After normalize")
+        # print("Before normalize")
+        # print(text)
+        # print("--------------------------------------------------------------------")
+        # print("After normalize")
+
         normalizer = Normalizer(token_based=True)
-        normaltext = normalizer.punctuation_spacing(normalizer.character_refinement(normalizer.affix_spacing(text)))
-        print(normaltext)
+        normal_text = normalizer.punctuation_spacing(normalizer.character_refinement(normalizer.affix_spacing(text)))
+
         write_text_file = open("./group8NormalizedText.txt", "wb")
-        write_text_file.write(normaltext.encode("utf-8"))
+        write_text_file.write(normal_text.encode("utf-8"))
         write_text_file.close()
 
-# testing for sobhan
+        return normal_text
+
+
+def split_paragraphs(text):
+
+    paragraphs = text.split("پاراگراف")[1:]
+
+    for i in range(len(paragraphs)):
+        paragraphs[i] = paragraphs[i][2:]
+
+    return paragraphs
+
 
 if __name__ == "__main__":
-    normalizing()
+    normalized_txt = normalizing()
+    paragraphs_list = split_paragraphs(normalized_txt)
