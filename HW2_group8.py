1bd355784fd4e4e543a541178d98c177a2c22df5
rezaBarzgar <r.barzgar98@gmail.com>
2020-10-04 17:12:50 +0330

    start

--- HW2_group8.py
+++ HW2_group8.py
@@ -1,16 +1,21 @@
 from __future__ import unicode_literals
 from hazm import *
-with open("./group8text.txt", 'r', encoding="utf8") as file:
-    text = file.read()
-    print("Before normalize")
-    print(text)
 
 
-    print("--------------------------------------------------------------------")
-    print("After normalize")
-    normalizer = Normalizer(token_based=True)
-    normaltext = normalizer.punctuation_spacing(normalizer.character_refinement(normalizer.affix_spacing(text)))
-    print(normaltext)
+def normalizing():
+    with open("./group8text.txt", 'r', encoding="utf8") as file:
+        text = file.read()
+        print("Before normalize")
+        print(text)
 
+        print("--------------------------------------------------------------------")
+        print("After normalize")
+        normalizer = Normalizer(token_based=True)
+        normaltext = normalizer.punctuation_spacing(normalizer.character_refinement(normalizer.affix_spacing(text)))
+        print(normaltext)
+        write_text_file = open("./group8NormalizedText.txt", "wb")
+        write_text_file.write(normaltext.encode("utf-8"))
+        write_text_file.close()
 
-
+if __name__ == "__main__":
+    normalizing()
