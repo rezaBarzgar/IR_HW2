9aaec2ffc75235043d696707f579955f4d68a28f
Armin eftekhari <armineftekharii77@gmail.com>
2020-10-04 15:59:31 +0330

    do punctuation spacing , refinement , affix spacing

--- HW2_group8.py
+++ HW2_group8.py
@@ -1,6 +1,16 @@
-import hazm
-
+from __future__ import unicode_literals
+from hazm import *
 with open("./group8text.txt", 'r', encoding="utf8") as file:
     text = file.read()
+    print("Before normalize")
+    print(text)
 
-print(text)  # ---- khob belakhare file txt ro darim, berim bra asl majera :))
+
+    print("--------------------------------------------------------------------")
+    print("After normalize")
+    normalizer = Normalizer(token_based=True)
+    normaltext = normalizer.punctuation_spacing(normalizer.character_refinement(normalizer.affix_spacing(text)))
+    print(normaltext)
+
+
+
